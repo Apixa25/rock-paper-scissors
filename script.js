f037124e413751cceff823e7015c504d20c57a8f
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-09-27 12:01:31 -0700

    commit 3 js script

---
+++ script.js
@@ -0,0 +1,8 @@
+function rockpaperscissors(){
+    console.log(" rock, paper, or scissors")
+
+    let rps= prompt("enter rock, paper, or scissors");
+    console.log (rps)
+    let lowercase= confirm("do you want to add a lowercase letter");
+    console.log (lowercase)
+}
