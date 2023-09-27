9870765802d66646c6381b33fa13ef4bdb91bfbb
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-09-27 12:16:41 -0700

    commit 5 added code to js

--- script.js
+++ script.js
@@ -3,6 +3,56 @@ function rockpaperscissors(){
 
     let rps= prompt("enter rock, paper, or scissors");
     console.log (rps)
-    let lowercase= confirm("do you want to add a lowercase letter");
-    console.log (lowercase)
+    let info= alert("You can only choose one")
+    let rock= confirm("do you want to add a lowercase letter");
+    console.log (rock)
+    let paper= confirm("do you want to add a uppercase letter");
+    console.log (paper)
+    let scissors= confirm("do you want to add a number");
+    console.log (scissors)
+ 
+    // added the charArray variable
+    var charArray = []
+    // made if statements for each part of the password
+    // had to add in each possible choice for the charArrays
+    if (rock == true ){
+     charArray += "R"
+    }
+    if (paper== true ){
+     charArray += "P"
+    } 
+    if (scissors == true ){
+     charArray += "S"
+    }
+    
+    // made a password variable 
+    // then put in a for loop
+   var game = ""
+   for (var x = 0; x < passwordLength; x++){
+     console.log (charArray)
+     var rannum = Math.random();
+     console.log (rannum)
+     var ranidx = rannum * charArray.length
+     console.log (ranidx)
+     var rouranidx = Math.floor(ranidx)
+     game += charArray [rouranidx] 
+   
+   return game
+   }
+   
+   
+   // Get references to the #generate element
+   var generateBtn = document.querySelector("#generate");
+   
+   // Write password to the #password input
+   function writePassword() {
+     var password = generatePassword();
+     var passwordText = document.querySelector("#password");
+   
+     passwordText.value = password;
+   
+   }
+   
+   // Add event listener to generate button
+   generateBtn.addEventListener("click", writePassword);
 }
